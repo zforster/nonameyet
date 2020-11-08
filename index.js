1bd766c3f02c205dd8abcd8e35f803be46d2a23e
zforster <zak.forster16@gmail.com>
2020-11-08 20:59:21 +0000

    get namespaces and rooms working

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.emit("welcome", "hello world!")
+io.of("/games").on("connection", (socket) => {  // create namespace
+    socket.on("joinRoom", (room) => { // client sends join room emit
+        console.log(`client joining room: ${room}`);
+        socket.join(room);  // let client join room
+        io.of("/games").in(room).emit("newUser", `new user is in your room ${room}`); // broadcast msg to games namespace use .on to broadcast to all but me, use .in to broadcast to all including me
+        return socket.emit("success", `joined room ${room}`) // send client a msg
+    })
 });
 
 http.listen(4000, () => {
