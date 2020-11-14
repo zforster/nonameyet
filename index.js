365c78a652a627479622ae53fa1fad964e0b0fea
zforster <zak.forster16@gmail.com>
2020-11-14 23:35:28 +0000

    start work on leaving room

--- index.js
+++ index.js
@@ -16,18 +16,26 @@ function getRhymes() {
 //     getRhymes().then((r) => res.send(r));
 // });
 
-
 io.of("/games").on("connection", (socket) => {
     socket.on("joinRoom", ({room, username}) => { // client sends join room emit
         console.log(`${username} joined ${room}`);
 
         socket["username"] = username;
+        socket["room"] = room;
         clients.push(socket);
 
         socket.join(room);
 
         socket.to(room).emit("userJoinedRoom", {username: socket["username"], id: socket.id});  // send the info of the user who just joined to everyone but the joiner
-        clients.forEach(client => {socket.emit("userJoinedRoom", {username: client["username"], id: client.id})});  // on join send emit usernames of everyone in the room to the client just joined
+        clients.forEach(c => {if(c["room"] === room){socket.emit("userJoinedRoom", {username: c["username"], id: c.id})}});  // on join send emit usernames of everyone in the room to the client just joined
+    });
+
+    socket.on("leaveRoom", (room) => { // client sends join room emit
+        console.log(`${socket.username} leaving room ${room}`);
+        // socket.to(room).emit("userLeftRoom", {username: socket["username"], id: socket.id});
+        // clients = clients.filter(function(el){  // remove disconnecting client from list
+        //     return el.id !== socket.id;
+        // });
     });
 
    // socket.on("sendMsg", ({username, msg, room}) => {
