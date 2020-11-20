63c0b10a9248d45cc0df74b00dc5add855f17389
zforster <zak.forster16@gmail.com>
2020-11-20 21:29:32 +0000

    communicate to the client their id and who is the game host

--- index.js
+++ index.js
@@ -17,20 +17,51 @@ function getRhymes() {
 //     getRhymes().then((r) => res.send(r));
 // });
 
+function getRoomHost(room){
+    // set host flag of the client who is connected and been in the room longest as the room host
+    let foundHost = false;
+    let host = null;
+    clients.forEach(client => {
+        if(client["room"] === room){
+            if(!foundHost){
+                client["host"] = true;
+                foundHost = true;
+                host = client;
+            }
+            else{
+                client["host"] = false;
+            }
+        }
+    });
+    return host;
+}
+
 io.of("/games").on("connection", (socket) => {
     socket.on("joinRoom", ({room, username}) => { // client sends join room emit
         socket["username"] = username;
         socket["room"] = room;
+
         clients.push(socket);
+        getRoomHost(room);
 
         socket.join(room);
 
-        socket.to(room).emit("userJoinedRoom", {username: socket["username"], id: socket.id});  // send the info of the user who just joined to everyone but the joiner
-        clients.forEach(c => {if(c["room"] === room){socket.emit("userJoinedRoom", {username: c["username"], id: c.id})}});  // on join send emit usernames of everyone in the room to the client just joined
+        socket.to(room).emit("userJoinedRoom", {username: socket["username"], id: socket.id, host: socket["host"]});  // send the info of the user who just joined to everyone but the joiner
+
+        clients.forEach(c => {
+            if(c["room"] === room){
+                if(c === socket){
+                    socket.emit("userJoinedRoom", {username: c["username"], id: c.id, host: c["host"], isYou: true});
+                }
+                else{
+                    socket.emit("userJoinedRoom", {username: c["username"], id: c.id, host: c["host"]});
+                }
+            }
+        });  // on join send emit usernames of everyone in the room to the client just joined
     });
 
     socket.on("disconnect", () => {
-        socket.to(socket.room).emit("userLeftRoom", {username: socket["username"], id: socket.id});
+        socket.to(socket.room).emit("userLeftRoom", {username: socket["username"], id: socket.id});  // send new host in response
         clients = clients.filter(function(el){  // remove disconnecting client from list
             return el.id !== socket.id;
         });
