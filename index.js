b3e4614cae020771353297e06552f6ae3a6b80f3
zforster <zak.forster16@gmail.com>
2020-11-20 21:38:52 +0000

    on user leave room set new host as the next person who joined after the origional host

--- index.js
+++ index.js
@@ -61,10 +61,12 @@ io.of("/games").on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
-        socket.to(socket.room).emit("userLeftRoom", {username: socket["username"], id: socket.id});  // send new host in response
         clients = clients.filter(function(el){  // remove disconnecting client from list
             return el.id !== socket.id;
         });
+        let host = getRoomHost(socket.room);
+
+        socket.to(socket.room).emit("userLeftRoom", {username: socket["username"], id: socket.id, host: host["id"]});  // send new host in response
     });
 
    //     socket.emit("broadcastMsg", {username, msg, room});// to just the client
